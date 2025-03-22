b12c8cf49e4e52688782c2e1f66c3968fd60c84a
Ali-Hamza-52 <alihamzabrw52@gmail.com>
2025-03-22 11:15:24 +0500

    Day 01 with functions in cpp

---
+++ 01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void displayName(string name){
+	cout<<"My name is ==> "<<name;
+}
+
+int main(){
+	displayName("Ali Hamza");
+	return 0;
+}
